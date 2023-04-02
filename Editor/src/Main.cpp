adb7b79573d72e64d4c051a52905e7c47fb90c13
George Fotopoulos <xorz57@gmail.com>
2023-04-02 20:43:17 +0300

    Update Main.cpp

--- Editor/src/Main.cpp
+++ Editor/src/Main.cpp
@@ -64,7 +64,7 @@ int main() {
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    auto clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    auto color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
@@ -73,8 +73,8 @@ int main() {
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        ImGui::Begin("Hello, world!");
-        ImGui::ColorEdit3("clear color", (float *) &clear_color);
+        ImGui::Begin("Hello, World!");
+        ImGui::ColorEdit3("Clear Color", (float *) &color);
         ImGui::End();
 
         ImGui::Render();
@@ -84,7 +84,7 @@ int main() {
         glfwGetFramebufferSize(window, &display_w, &display_h);
         glViewport(0, 0, display_w, display_h);
 
-        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClearColor(color.x, color.y, color.z, color.w);
         glClear(GL_COLOR_BUFFER_BIT);
 
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
